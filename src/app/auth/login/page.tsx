2ff672b89548e48d78ccf53f4045222af7f96c29
Gaurav Singh karki <karkigauravsingh@gmail.com>
2024-05-30 22:36:36 +0530

    auth add

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from "@/components/sign-in"
+
+const page = () => {
+  return (
+    <div>
+      <SignIn/>
+    </div>
+  )
+}
+
+export default page
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
 
                          <Link href='/configure/upload' className={buttonVariants({
                             size: 'sm',
-                            variant: 'hidden sm:flex items-center gap-1',
+                            className: 'hidden sm:flex items-center gap-1',
                          })}>Create Case
                          <ArrowRight className="ml-1.5 h-5 w-5"/>
                          </Link>
@@ -43,19 +43,19 @@ const Navbar = () => {
                             variant: 'ghost',
                          })}>Sign up</Link>
 
-                        <Link href='/api/auth/login' className={buttonVariants({
+                        <Link href='/auth/login' className={buttonVariants({
                             size: 'sm',
                             variant: 'ghost',
                          })}>Login</Link>
 
-                         <div className="h-8 w-px bg-zinc-200 hidden sm:block">
-                         <Link href='/configure/upload' className={buttonVariants({
-                            size: 'sm',
-                            variant: 'hidden sm:flex items-center gap-1',
-                         })}>Create Case
-                         <ArrowRight className="ml-1.5 h-5 w-5"/>
-                         </Link>
-                         </div>
+                         <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+                           <Link href='/configure/upload' className={buttonVariants({
+                              size: 'sm',
+                              className: 'hidden sm:flex items-center gap-1',
+                           })}>Create Case
+                           <ArrowRight className="ml-1.5 h-5 w-5"/>
+                           </Link>
+                         {/* </div> */}
 
                        </>
                     )
