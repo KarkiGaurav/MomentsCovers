58801835af8d9933e0876bbb5078835afa495e4f
Gaurav Singh Karki <karkigauravsingh@gmail.com>
2024-07-03 20:19:55 +0530

    Image uploading: added images uploading feature with the help of dropzone and uploadthing

---
+++ src/app/configure/upload/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import { Progress } from "@/components/ui/progress"
+import { cn } from "@/lib/utils"
+import { Image, Loader, MousePointerSquareDashed } from "lucide-react"
+import { useState, useTransition } from "react"
+import Dropzone, {FileRejection} from 'react-dropzone'
+
+const Page = () => {
+
+    const [isDragOver, setIsDragOver] = useState<Boolean>(false)
+    const [uploadProgrss, setUploadProgrss] = useState<number>(0)
+
+    const onDropRejected = () => {}
+    const onDropAccepted = () => {
+        console.log("working")
+    }
+
+    const isUploading = false;
+    const [isPending, startTransition ] = useTransition()
+
+    return (
+        <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center", {
+            'ring-blue-900/25 bg-blue-900/10': isDragOver,
+        })}>
+          <div className="relative flex flex-l flex-col items-center justify-center w-full">
+             <Dropzone onDropRejected={onDropRejected} onDropAccepted={onDropAccepted} accept={{
+                "images/png": [".png"],
+                "images/jpeg": [".jpeg"],
+                "images/jpg": [".jpg"],
+             }} 
+             onDragEnter={() => setIsDragOver(true)}
+             onDragLeave={() => setIsDragOver(false)}
+             >
+              {
+                 ({getRootProps, getInputProps})=> (
+                    <div className="h-full w-full flex-1 flex flex-col items-center justify-center"  {...getRootProps()}>
+                       <input {...getInputProps()} />
+                       {isDragOver? (<MousePointerSquareDashed className='h-6 w-6 text-zinc-500 mb-2'/>): isUploading || isPending ? (
+                        <Loader className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
+                       ):(
+                        <Image className="h-6 w-6 text-zinc-600 mb-2"/>
+                       )}
+
+                       <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700 ">
+                          { isUploading? (
+                              <div className="flex flex-col items-center">
+                                 <p>Uploading...</p>
+                                 <Progress value={uploadProgrss} className="bg-gray-300 mt-2 w-40 h-2" />
+                              </div>
+                           ): isPending? (
+                              <div  className="flex flex-col items-center"> Redirecting, please wait... </div>
+                           ): isDragOver? (
+                              <p>
+                               <span className="font-semibold"> Drop file </span> to upload  
+                              </p>
+                           ): (
+                              <p>
+                                 <span className="font-semibold"> Click to upload  </span> or drag and drop  
+                              </p>
+                           )
+                          }
+                       </div>
+                       {isPending? null: (
+                        <p className="text-xs text-zinc-500">PNG, JPG JPEG</p>
+                       )}
+                    </div>
+                 )
+              }
+             </Dropzone>
+          </div>
+        </div>
+    )
+}
+
+export default Page
--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react"
 
 const MaxWidthWrapper = ({ className, children, }: { className?: string; children: ReactNode; }) => {
     return (
-        <div className={cn('h-full mx-auto max-w-screen-xl md:px-20', className)}>
+        <div className={cn('h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
             {children}
         </div>
     )
