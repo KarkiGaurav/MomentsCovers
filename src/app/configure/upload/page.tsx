db5a81145c9b53cd099fbd6b68535e18c802991f
Gaurav Singh Karki <karkigauravsingh@gmail.com>
2024-07-04 22:30:58 +0530

    Image uploading part done

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,22 +1,50 @@
 "use client"
 
 import { Progress } from "@/components/ui/progress"
+import { useToast } from "@/components/ui/use-toast"
+import { useUploadThing } from "@/lib/uploadthing"
 import { cn } from "@/lib/utils"
 import { Image, Loader, MousePointerSquareDashed } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { useState, useTransition } from "react"
-import Dropzone, {FileRejection} from 'react-dropzone'
+import Dropzone, { FileRejection } from 'react-dropzone'
 
 const Page = () => {
 
+   const toast = useToast()
     const [isDragOver, setIsDragOver] = useState<Boolean>(false)
     const [uploadProgrss, setUploadProgrss] = useState<number>(0)
+    const router = useRouter()
 
-    const onDropRejected = () => {}
-    const onDropAccepted = () => {
-        console.log("working")
+  const {startUpload, isUploading} = useUploadThing('imageUploader', {
+   onClientUploadComplete: ([data]) => {
+      const configId = data.serverData.configId
+      startTransition(() => {
+         router.push(`configure/design?id=${configId}`)
+      })
+   },
+   onUploadProgress(p){
+      setUploadProgrss(p)
+   },
+  })
+
+    const onDropRejected = (rejectedFiles: FileRejection[]) => {
+      const [file] = rejectedFiles
+
+      setIsDragOver(false)
+      toast({
+         title:  `${file.file.type} type is not supported`,
+         description : "Please choose a PNG, JPG, or JPEG image instead",
+         variant: 'destructive'
+       })
     }
 
-    const isUploading = false;
+    const onDropAccepted = (acceptedFiles: File[]) => {
+       startUpload(acceptedFiles, {configId: undefined})
+
+       setIsDragOver(false)
+    }
+
     const [isPending, startTransition ] = useTransition()
 
     return (
---
+++ src/components/Steps.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation"
+
+const STEPS = [
+    {
+        name: 'Step 1: Add image',
+        description: 'Choose an image for your case',
+        url: '/upload'
+    },
+    {
+        name: 'Step 2: Customize design',
+        description: 'Make the case yours',
+        url: '/design'
+    },
+    {
+        name: 'Step 3: Summary',
+        description: 'Review your final design',
+        url: '/preview'
+    },
+]
+
+const Steps = () => {
+
+    const pathname = usePathname();
+    return ( 
+        <ol className="rounded-md bg-white lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
+           {STEPS.map((step, i)=>{
+              const isCurrent = pathname.startsWith(step.url)
+              const isCompleted = STEPS.slice(i+1).some((step) => 
+                pathname.endsWith(step.url)
+              )
+
+              const imgPath = `/snake-${i+1}.png`
+              return (
+                <li key={step.name} className="relative overflow-hidden lg:flex-1">
+                    <span className={cn('absolute left-0 top-0 h-full w-1 bg-zinc-400 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full', {
+                        'bg-zinc-700': isCurrent,
+                        'bg-primary': isCompleted,
+                    })} 
+                    aria-hidden='true'>
+ 
+                        <span className={cn(i !== 0 ? 'lg:pl-9': '', 'flex items-center px-6 py-4 text-sm font-medium')}>
+
+                            <img src={imgPath} alt="snale" className="h-6"/>
+                        </span>
+
+
+                    </span>
+
+                </li>
+              )
+           })}
+        </ol>
+    )
+}
+
+export default Steps
