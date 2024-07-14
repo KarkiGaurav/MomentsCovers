24f54f8686853cd8abe1ea6253d351fb133f664c
Gaurav Singh Karki <karkigauravsingh@gmail.com>
2024-07-14 22:41:41 +0530

    DesignPreview components added

---
+++ src/app/configure/preview/DesignPreview.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import Phone from "@/components/Phone"
+import { BASE_PRICE } from "@/config/products"
+import { cn, formatPrice } from "@/lib/utils"
+import { COLORS, MODELS } from "@/validators/option-validator"
+import { Configuration } from "@prisma/client"
+import { Check } from "lucide-react"
+import { useEffect, useState } from "react"
+import Confetti from 'react-dom-confetti'
+
+const DesignPreview = ({ configuration}: {configuration: Configuration}) => {
+
+    const [showConfetti, setShowConfetti] = useState<boolean>(false)
+    useEffect(() => setShowConfetti(true))
+
+    const { color, material, model, finish, croppedImageUrl } = configuration
+
+    const tw = COLORS.find((supportedColor) => supportedColor.value === color)?.tw
+
+    const { label: modelLabel} = MODELS.options.find(({ value }) => value === model)!
+
+
+  return (
+    <>
+        <div className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center" >
+            <Confetti active={ showConfetti} config={{ elementCount: 200, spread: 90}} />
+        </div>
+
+        <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
+            <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
+                <Phone className={cn(`bg-${tw}, "max-w-[150px] md:max-w-full"`)} imgSrc={croppedImageUrl!} />
+            </div>
+
+            <div className="mt-9 sm:col-span-9 md:row-end-1">
+                <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+                    Your { modelLabel } Case
+                </h3>
+                <div className="mt-3 flex items-center gap-1.5 text-base">
+                    <Check className="w-4 h-4 text-green-500"/>
+                    In stock and ready to ship 
+                </div>
+            </div>
+
+            <div className="sm:col-span-12 md:col-span-9 text-base">
+                <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+                    <div>
+                        <p className="font-medium text-zinc-950">Highlights</p>
+                        <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                            <li>Wireless charging compatible</li>
+                            <li>TPU shock absorption</li>
+                            <li>Packking made from recyled materials</li>
+                            <li>5 year print warranty</li>
+                        </ol>
+                    </div>
+
+                    <div>
+                        <p className="font-medium text-zinc-950">Material</p>
+                        <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                            <li> High quality durable material</li>
+                            <li>Scratch and fingerprint resistant coating</li>
+                            </ol>
+                    </div>
+                </div>
+
+                <div className="mt-8">
+                    <div className="bg-gray-50 p-6 sm:rounded-lg sm:p-8">
+                        <div className="flow-root text-sm">
+                            <div className="flex items-center justify-between py-1 mt-2">
+                                <p className="text-gray-600">Base price</p>
+                                <p className="font-medium text-gray-900">{formatPrice(BASE_PRICE / 100)}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </>
+
+  )
+}
+
+export default DesignPreview
--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -12,7 +12,7 @@ const Phone = ({imgSrc, className, dark=false, ...props}: PhoneProps) => {
         <img src={dark? '/phone-template-dark-edges.png': '/phone-template-white-edges.png'} className="pointer-events-none z-50 select-none" alt="phone image" />
 
          <div className="absolute -z-10 inset-0">
-            <img src={imgSrc} className="object-cover" alt="overlaying phone image"/>
+            <img src={imgSrc} className="object-cover min-w-full min-h-full" alt="overlaying phone image"/>
          </div>
        </div>
     )
